f3a0e95a28d6ccaa95eada65846f5ae9b753383a
Michael Osuna Jr <cbosuna@gmail.com>
2009-01-17 12:16:37 -0800

    fixed test typo

--- js/qunit.core.js
+++ js/qunit.core.js
@@ -2,6 +2,6 @@ module("core");
 
 test("Basic requirements", function(){
 	ok($.controller.array, "$.controller.array");
-	ok($.controller.object, "$.controller.array");
+	ok($.controller.object, "$.controller.object");
 	ok($.controller.defaults, "$.controller.defaults");
 });
