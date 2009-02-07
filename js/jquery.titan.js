97e9aefb7e5f19181ba02a3fff97c842e1ae3257
willwilson <Will Wilson@.(none)>
2009-02-07 11:16:58 -0800

    added formatNumber()

--- js/jquery.titan.js
+++ js/jquery.titan.js
@@ -724,6 +724,58 @@
 		});
 	}
 
+	function number_format( number, decimals, dec_point, thousands_sep ) {
+		// http://kevin.vanzonneveld.net
+		// +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+		// +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+		// +     bugfix by: Michael White (http://getsprink.com)
+		// +     bugfix by: Benjamin Lupton
+		// +     bugfix by: Allan Jensen (http://www.winternet.no)
+		// +    revised by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+		// +     bugfix by: Howard Yeend
+		// +    revised by: Luke Smith (http://lucassmith.name)
+		// +     bugfix by: Diogo Resende
+		// *     example 1: number_format(1234.56);
+		// *     returns 1: '1,235'
+		// *     example 2: number_format(1234.56, 2, ',', ' ');
+		// *     returns 2: '1 234,56'
+		// *     example 3: number_format(1234.5678, 2, '.', '');
+		// *     returns 3: '1234.57'
+		// *     example 4: number_format(67, 2, ',', '.');
+		// *     returns 4: '67,00'
+ 
+		var n = number, prec = decimals, dec = dec_point, sep = thousands_sep;
+		n = !isFinite(+n) ? 0 : +n;
+		prec = !isFinite(+prec) ? 0 : Math.abs(prec);
+		sep = sep == undefined ? ',' : sep;
+ 
+		var s = n.toFixed(prec),
+			abs = Math.abs(n).toFixed(prec),
+			_, i;
+ 
+		if (abs > 1000) {
+			_ = abs.split(/\D/);
+			i = _[0].length % 3 || 3;
+
+			_[0] = s.slice(0,i + (n < 0)) +
+				_[0].slice(i).replace(/(\d{3})/g, sep+'$1');
+
+			s = _.join(dec || '.');
+		} else {
+			s = abs.replace('.', dec_point);
+		}
+ 
+		return s;
+	}
+
+	$.fn.formatNumber = function(number, decimals, dec_point, thousands_sep) {
+		return $(this).format(function(elem, data){
+			var val = $(data).valueForKey(number);
+			dec_point = dec_point == undefined ? '.' : dec_point;
+			$(elem).text(number_format(val, decimals, dec_point, thousands_sep));
+		});
+	}
+
 	$.fn.formatForm = function(controller, options){
 		return $(this).format(function(elem, data){
 			$(elem).submit(function(event){
