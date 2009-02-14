cb34733aa7eddda157b75ba380174390e63f3b9f
willwilson <Will Wilson@.(none)>
2009-02-13 18:04:20 -0800

    added /index.php to the end of the default url

--- js/qunit.controller.js
+++ js/qunit.controller.js
@@ -1,6 +1,6 @@
 module("controller", {
 	setup: function() {
-		$.controller.defaults.url = "server/";
+		$.controller.defaults.url = "server/index.php";
 		$.ajaxSetup({timeout: 0});
 	},
 	teardown: function() {
