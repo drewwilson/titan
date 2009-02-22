541ec0cdd85dce4d23e56dc98cf8d2db79a1ba74
Michael Osuna Jr <cbosuna@gmail.com>
2009-02-22 01:18:14 -0800

    revamped the server code

--- server/index.php
+++ server/index.php
@@ -4,78 +4,79 @@ require('compat.php');
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-if ( ! file_exists("data")) {
-	mkdir("data");
-}
-$params = array_merge($_GET, $_POST);
-
-if (array_key_exists("_method", $params)) {
-	$method = $params['_method'];
-	unset($params['_method']);
-} else {
-	$method = $_SERVER['REQUEST_METHOD'];
-}
-$segs = split("/", $_SERVER['PATH_INFO']);
-$key = $segs[1];
-$params = $_GET;
-if (array_key_exists(2, $segs)) {
-	$params['id'] = $segs[2];
+function getRequest()
+{
+	if (in_array("application/json", explode(",", $_SERVER['HTTP_ACCEPT']))) {
+		header('Content-type: application/json');
+	}
+	$request = new stdClass();
+	$request->params = array_merge($_GET, $_POST);
+	if (array_key_exists("_method", $request->params)) {
+		$request->method = $params['_method'];
+		unset($request->params['_method']);
+	} else {
+		$request->method = $_SERVER['REQUEST_METHOD'];
+	}
+	if ($request->method == "PUT") {
+		parse_str(file_get_contents('php://input'), $_PUT);
+		foreach ($_PUT as $prop => $val) {
+			$request->params[$prop] = $val;
+		}
+	}
+	$segs = split("/", $_SERVER['PATH_INFO']);
+	$request->key = $segs[1];
+	$request->count = false;
+	if (array_key_exists(2, $segs)) {
+		if ($segs[2] == "count") {
+			$request->count = true;
+		} else {
+			$params['id'] = $segs[2];
+		}
+	}
+	if ( ! file_exists("data")) {
+		mkdir("data");
+	}
+	if ( ! file_exists("data/{$request->key}.json")) {
+		copy("fixtures/{$request->key}.json", "data/{$request->key}.json");
+		chmod("data/{$request->key}.json", 0666);
+	}
+	$request->data = file_get_contents("data/{$request->key}.json");
+	$request->data = json_decode($request->data);
+	return $request;
 }
 
 function cmp($one, $two)
 {
-	if ($one->position == $two->position) {
-		return 0;
-	} else if ($one->position < $two->position) {
-		return -1;
-	} else {
-		return 1;
-	}
+	$one = $one->position;
+	$two = $two->position;
+	return $one == $two ? 0 : $one < $two ? -1 : 1;
 }
 
-switch($method) {
+$request = getRequest();
+
+switch($request->method) {
 	case "GET":
-		$segs = split("/", $_SERVER['PATH_INFO']);
-		$key = $segs[1];
-		$params = $_GET;
-		$doCount = false;
-		if (array_key_exists(2, $segs)) {
-			if ($segs[2] == "count") {
-				$doCount = true;
-			} else {
-				$params['id'] = $segs[2];
-			}
-		}
-		if ( ! file_exists("data/$key.json")) {
-			copy("fixtures/$key.json", "data/$key.json");
-			chmod("data/$key.json", 0666);
-		}
-		$data = file_get_contents("data/$key.json");
-		$data = json_decode($data);
 		$newdata = array();
 		$keepit = true;
-		if (array_key_exists("order", $params)) {
-			usort($data, "cmp");
-			unset($params["order"]);
+		if (array_key_exists("order", $request->params)) {
+			usort($request->data, "cmp");
+			unset($request->params["order"]);
 		}
-		foreach(array_keys($data) as $idx) {
-			if ( ! empty($params) && is_array($params)) {
-				foreach($params as $name => $value) {
-					if ($data[$idx]->{$name} != $value) {
+		foreach(array_keys($request->data) as $idx) {
+			if ( ! empty($request->params) && is_array($request->params)) {
+				foreach($request->params as $name => $value) {
+					if ($request->data[$idx]->{$name} != $value) {
 						$keepit = false;
 					}
 				}
 			}
 			if ($keepit) {
-				$newdata[] = $data[$idx];
+				$newdata[] = $request->data[$idx];
 			}
 			$keepit = true;
 		}
-		if (in_array("application/json", explode(",", $_SERVER['HTTP_ACCEPT']))) {
-			header('Content-type: application/json');
-		}
 		if (count($newdata) > 0) {
-			if ($doCount) {
+			if ($request->count) {
 				echo count($newdata);
 			} else {
 				echo json_encode($newdata);
@@ -85,67 +86,56 @@ switch($method) {
 		}
 		break;
 	case "POST":
-		$data = file_get_contents("data/$key.json");
-		$data = json_decode($data);
-		$data[] = (object)$params;
-        
-		$f = fopen("data/$key.json", "w");
-		fwrite($f, json_encode($data));
+		echo " ";
+		$f = fopen("data/{$request->key}.json", "w");
+		$request->data[] = $request->data;
+		fwrite($f, json_encode($request->data));
 		fclose($f);
 		break;
 	case "PUT":
-		parse_str(file_get_contents('php://input'), $_PUT);
-		foreach ($_PUT as $prop=>$val) {
-			$params[$prop] = $val;
-		}
-		$data = file_get_contents("data/$key.json");
-		$data = json_decode($data);
 		$newdata = array();
-		if (array_key_exists("position", $params)) {
-			$new_pos = $params['position'];
+		if (array_key_exists("position", $request->params)) {
+			$new_pos = $request->params['position'];
 			$old_pos = -1;
-			foreach(array_keys($data) as $idx) {
-				if ($data[$idx]->id == $params['id']) {
-					$old_pos = $data[$idx]->position;
+			foreach(array_keys($request->data) as $idx) {
+				if ($request->data[$idx]->id == $request->params['id']) {
+					$old_pos = $request->data[$idx]->position;
 				} 
 			}
-			foreach(array_keys($data) as $idx) {
-				$pos = $data[$idx]->position;
+			foreach(array_keys($request->data) as $idx) {
+				$pos = $request->data[$idx]->position;
 				if ($old_pos > $new_pos) {
 					if($pos >= $new_pos && $pos < $old_pos) {
-						$data[$idx]->position += 1;
+						$request->data[$idx]->position += 1;
 					}
 				} else {
 					if($pos > $old_pos && $pos <= $new_pos) {
-						$data[$idx]->position -= 1;
+						$request->data[$idx]->position -= 1;
 					}
 				}
 			}
 		}
-		foreach(array_keys($data) as $idx) {
-			if ($data[$idx]->id == $params['id']) {
-				$newdata[] = (object)array_merge((array)$data[$idx], $params);
+		foreach(array_keys($request->data) as $idx) {
+			if ($request->data[$idx]->id == $request->params['id']) {
+				$newdata[] = (object)array_merge((array)$request->data[$idx], $request->params);
 			} else {
-				$newdata[] = $data[$idx];
+				$newdata[] = $request->data[$idx];
 			}
 		}
-		$f = fopen("data/$key.json", "w");
+		$f = fopen("data/{$request->key}.json", "w");
 		fwrite($f, json_encode($newdata));
 		fclose($f);
 		break;
 	case "DELETE":
-		$data = file_get_contents("data/$key.json");
-		$data = json_decode($data);
 		$newdata = array();
-		foreach(array_keys($data) as $idx) {
-			if ($data[$idx]->id != $params['id']) {
-				$newdata[] = $data[$idx];
+		foreach(array_keys($request->data) as $idx) {
+			if ($request->data[$idx]->id != $request->params['id']) {
+				$newdata[] = $request->data[$idx];
 			}
 		}
-		$f = fopen("data/$key.json", "w");
+		$f = fopen("data/{$request->key}.json", "w");
 		fwrite($f, json_encode($newdata));
 		fclose($f);
-		
 		break;
 }
 
